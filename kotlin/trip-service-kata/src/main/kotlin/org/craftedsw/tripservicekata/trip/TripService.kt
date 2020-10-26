9ded955ef235400ab5885a3c288f24a7a8c66422
Ricardo Bevilacqua <bevilacqua.ricardo@gmail.com>
2020-10-26 15:29:36 -0300

    SOLVE-KOTLIN Adds some tests

--- kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
+++ kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
@@ -5,26 +5,34 @@ import org.craftedsw.tripservicekata.user.User
 import org.craftedsw.tripservicekata.user.UserSession
 import java.util.*
 
-class TripService {
+open class TripService {
 
     fun getTripsByUser(user: User): List<Trip> {
+
         var tripList: List<Trip> = ArrayList<Trip>()
-        val loggedUser: User? = UserSession.instance.loggedUser
+        val loggedUser: User? = getLoggedUser()
         var isFriend: Boolean = false
+
         if (loggedUser != null) {
+
             for (friend in user.friends) {
                 if (friend == loggedUser) {
                     isFriend = true
                     break
                 }
             }
+
             if (isFriend) {
                 tripList = TripDAO.findTripsByUser(user)
             }
+
             return tripList
+
         } else {
             throw UserNotLoggedInException()
         }
     }
+
+    open fun getLoggedUser() = UserSession.instance.loggedUser
 
 }
--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -1,5 +1,80 @@
 package org.craftedsw.tripservicekata.trip
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
+import org.craftedsw.tripservicekata.user.User
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.util.*
+
 class TripServiceTest {
+
+    val ANY_USER = User()
+
+    private lateinit var tripService: TripServiceTestable
+
+    private var user: User = User()
+    private var loggedUser: User? = User()
+
+    private var tripList: List<Trip> = ArrayList<Trip>()
+
+    @Test
+    fun `should throw user not logged exception when user is not logged`(){
+        givenNotLoggedInUser()
+
+        assertThrows<UserNotLoggedInException> {
+            whenGetTripsByUserIsCalled()
+        }
+    }
+
+    @Test
+    fun `should return empty trip list when user has no friends`(){
+        givenLoggedUser()
+        givenUserHasNoFriends()
+
+        whenGetTripsByUserIsCalled()
+
+        thenTripListShouldBeEmpty()
+    }
+
+    @Test
+    fun `should return empty trip list when user is not friends with logged user`(){
+        givenLoggedUser()
+        givenUserIsFriendsWith(ANY_USER)
+
+        whenGetTripsByUserIsCalled()
+
+        thenTripListShouldBeEmpty()
+    }
+
+    private fun givenNotLoggedInUser(){
+        loggedUser = null
+        tripService = TripServiceTestable(loggedUser)
+    }
+
+    private fun givenLoggedUser(){
+        loggedUser = User()
+        tripService = TripServiceTestable(loggedUser)
+    }
+
+    private fun givenUserHasNoFriends() {
+        user = User()
+    }
+
+    private fun givenUserIsFriendsWith(anyUser: User) {
+        user.addFriend(anyUser)
+    }
+
+    private fun whenGetTripsByUserIsCalled() {
+        tripList = tripService.getTripsByUser(user)
+    }
+
+    private fun thenTripListShouldBeEmpty() {
+        assertEquals(Collections.emptyList<Trip>(), tripList)
+    }
+
+    class TripServiceTestable(private var loggedUser: User?) : TripService(){
+        override fun getLoggedUser() = loggedUser
+    }
 
 }
